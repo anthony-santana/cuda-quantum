f92fcfe17bfc698f0b07347113d9b82b9ff0f573
A.M. Santana <anthonys@nvidia.com>
2024-05-14 19:57:57 +0000

    more progress
    Signed-off-by: A.M. Santana <anthonys@nvidia.com>

--- docs/sphinx/examples/python/dynamic_simulation/neutral_atoms_energy_minimization.py
+++ docs/sphinx/examples/python/dynamic_simulation/neutral_atoms_energy_minimization.py
@@ -8,35 +8,16 @@ import functools
 import itertools
 from multiprocess import Pool
 
-# import optax
-# import tensorflow as tf
-# import tensorflow_probability as tfp
-
 import cudaq
 
-# TODO:
-# 1. Speed up certain calculations in parallel
-# 2. Get rid of inneficiencies in certain calculations
-#    (such as how I pack and unpack params)
-# 3. Move linear algebra to cupy for speed up
-# 4. Make the actual qubit positions an optimizable parameter
-# 5. Create data types to make the code more generally understandable
-# 6. Model their common sources of noise within the Hamiltonian to create
-#    a more realistic simulation.
-# 7. Translate all of this to C++ code written in CUDA
-# 8. Area underneath signal should be angle of single qubit rotation
 
-# NEED TO Have an (x,y) coordinate pair for each qubit
-# This will be used to do the distance calcualtions between
-# them for the interaction terms.
+# Find the signals, phases, and detunings for N-Rydberg atoms in an
+# array that minimizes the energy expectation value of the system.
 
-# When thinking about rewriting with CUDA in C++, much of
-# these calculations can just stay on the GPU and all I should
-# need to bring back to the CPU is the actual cost value.
 
 ######################################### Timing Parameters ###################################################
 
-T = 1.5  # total time, T in microseconds.
+T = 2.5  # total time, T in microseconds.
 dt = 0.25  # time duration of each signal chunk in microseconds.
 chunks = int(T / dt)  # number of time chunks we will solve for
 
@@ -131,6 +112,9 @@ def unitary_step(time_step, amplitudes, phases, detunings):
     time = dt * time_step
     U_slice = sp.linalg.expm(-1j * dt *
                              hamiltonian(amplitudes, phases, detunings))
+    # TODO: Would like to register the unitaries with CUDA-Q here to avoid
+    # having to loop through every matrix again, but it goes out of scope
+    # after return.
     return U_slice
 
 
@@ -177,61 +161,37 @@ def parallel_unitary_evolution(signals: np.ndarray, phases: np.ndarray,
     pool.close()
     pool.join()
 
-    # Multiply through all unitary slices to get full time evolution.
-    U_T = functools.reduce(np.matmul, unitary_matrices)
-
-    # print(f"U(T) =\n {np.matrix(np.round(np.abs(U_T), decimals=5))}")
-    return U_T
-
-
-################################################################################################################
+    initial_name = "custom_operation_"
+    for unitary, time_step in zip(unitary_matrices, time_steps):
+      cudaq.register_operation(unitary, operation_name=initial_name + str(time_step))
+    return cudaq.globalRegisteredUnitaries.keys()
 
 
-def calculate_gate_fidelity(want_gate, got_gate):
-    """
-    Returns the overlap between the desired gate and the
-    evolved gate. A value of 0.0 represents no overlap 
-    and 1.0 represents perfect overlap.
-
-    F = (1/(d**2)) * | tr(U_want^dag * U_got) | **2
-
-    where d is the dimension of our gate
-    """
-    fidelity = (1 / want_gate.size) * (np.abs(
-        np.trace(np.dot(np.conj(want_gate).T, got_gate)))**2)
-    print(f"fidelity = {fidelity}")
-    return fidelity
-
-
-def calculate_state_fidelity(want_state, got_state):
-    """
-    Returns the overlap between the desired state and the
-    evolved state. A value of 0.0 represents no overlap 
-    (orthogonal states), and 1.0 represents perfect overlap.
-
-    F = |<psi_want | psi_got>| ** 2
-    """
-    fidelity = np.abs(np.dot(np.conj(want_state).T, got_state))**2
-    return fidelity
+def get_energy_expectation(unitary_operation_names):
+    kernel = cudaq.make_kernel()
+    qubits = kernel.qalloc(qubit_count)
+    # Loop through and apply the registered unitary operations
+    # to the kernel.
+    for unitary_operation in unitary_operation_names:
+        execution_string = f'''
+qubit_args = []
+for qubit_index in range({qubits.size()}):
+  qubit_args.append(qubits[qubit_index])
+kernel.{unitary_operation}(*qubit_args)
+'''
+        exec(execution_string)
+    # Sample the kernel and return its expectation value.
+    return cudaq.sample(kernel).expectation()
 
 
 ################################################################################################################
 
-
-def optimization_function(parameters: np.ndarray, *args):
+def optimization_function(parameters: np.ndarray):
     # Flipping these ahead of time for time ordering, since they'll all
     # be nested away and more inconvenient to flip then.
     # numpy_parameters = parameters.numpy()
     # flipped_parameters = np.flip(numpy_parameters)
     flipped_parameters = np.flip(parameters)
-    # TODO: Write this code in a more efficient manner. There's a lot of data
-    #       splitting, merging, stacking, etc. that could be avoided with better
-    #       code.
-    # The flattened array of parameters comes into us with the leftmost entry
-    # being the first sample of the signal on qubit 0, and the rightmost
-    # entry being the last sample of the detuning "signal" on qubit N.
-    # To properly time order these when simulating time evolution, we flip the
-    # parameters array and partition the parameters to their respective location.
     detunings = np.split(flipped_parameters[0:qubit_count * chunks],
                          qubit_count)
     phases = np.split(
@@ -247,28 +207,25 @@ def optimization_function(parameters: np.ndarray, *args):
     stacked_phases = np.column_stack(tuple(phases))
     stacked_signal_samples = np.column_stack(tuple(signals))
 
-    # Get the evolved unitary matrix.
-    got_gate = parallel_unitary_evolution(stacked_signal_samples,
-                                          stacked_phases, stacked_detunings,
-                                          chunks)
+    # Get the name for each unitary time step that we will
+    # apply to our system.
+    unitary_operations = parallel_unitary_evolution(stacked_signal_samples,
+                                                stacked_phases, stacked_detunings,
+                                                chunks)
 
-    # Calculate the fidelity and return it as a cost (1 - fidelity)
-    # so that our optimizer can minimize the function.
-    cost = 1. - calculate_gate_fidelity(gate_to_optimize, got_gate)
-    print(f"cost = {cost}")
-    # return tf.cast(cost, tf.float64)
-    return cost
+    print("energy = ", get_energy_expectation(unitary_operations))
+
+    return get_energy_expectation(unitary_operations)
 
 
-def run_optimization(want_gate: np.ndarray):
+
+def run_optimization(_qubit_count: int):
     """
     Closed loop optimization of the control signals:
     Will run the optimization for the provided unitary operation
     """
-    global gate_to_optimize
-    gate_to_optimize = want_gate
     global qubit_count
-    qubit_count = int(np.log2(want_gate.shape[0]))
+    qubit_count = _qubit_count
 
     # TODO:
     # 1. Lessen the parameter landscape by picking a fixed value
@@ -307,131 +264,16 @@ def run_optimization(want_gate: np.ndarray):
     initial_controls = np.concatenate(
         (initial_signal, initial_phases, initial_detunings))
 
-    # initial_controls_cast = tuple([tf.constant(value, tf.float64) for value in initial_controls])
-    # # Optimizing as one large variable that I manually unpack into
-    # # the proper place. This is to keep this script compatible with
-    # # scipy optimizers, but could be reworked at some point.
-    # controls_to_optimize = tf.Variable(initial_controls)
-    # print(controls_to_optimize)
-    # optim_results = tfp.optimizer.differential_evolution_minimize(
-    #     optimization_function,
-    #     initial_position=initial_controls_cast,
-    #     seed=43210)
-    # print(optim_results.converged)
-
     optimized_result = optimize.minimize(optimization_function,
                                          initial_controls,
-                                         args=(want_gate),
                                          bounds=bounds,
-                                        #  method="SLSQP")
-                                        #  method="trust-constr")
                                          method="Nelder-Mead")
 
-    # # optimized_result = optimize.dual_annealing(
-    # #     func=optimization_function,
-    # #     x0=initial_controls,
-    # #     bounds=bounds)#,
-    # #     # visit=1.25,
-    # #     # no_local_search=True)
-    # return optimized_result
+    return optimized_result
 
 
-def broadcast_single_qubit_operation(single_qubit_operation: np.ndarray,
-                                qubit_count: int):
-    """
-    Returns the matrix for a single-qubit gate, broadcast
-    to be applied to the entire Hilbert space.
-    """
-    return functools.reduce(np.kron, [single_qubit_operation] * qubit_count)
+######################################## Minimize Energy #######################################################
 
-
-def broadcast_individual_operations(operations: list[np.ndarray]):
-    # TODO: Need to broadcast each against identity operations
-    # before I add them each up.
-    full_gate = []
-    single_qubit_identity = np.eye(2)
-    broadcast_list = [single_qubit_identity] * len(operations)
-    print("operations = ", operations)
-    print(broadcast_list)
-    for operation_index, operation in enumerate(operations):
-        print("index = ", operation_index)
-        broadcast_list[operation_index] = operation
-        print(broadcast_list)
-        # print(functools.reduce(np.kron, broadcast_list))
-        full_gate.append(functools.reduce(np.kron, broadcast_list))
-        broadcast_list[operation_index] = single_qubit_identity
-    return sum(full_gate)
-
-
-################################################################################################################
-
-qubit_count = 5
-
-######################################## X-180 rotation of system ##############################################
-
-# # Now let's optimize for an X-rotation
-# x_180_rotation = np.fliplr(np.eye(2**qubit_count))
-# x_180_result = run_optimization(x_180_rotation)
-# x_180_signal = x_180_result.x
-# print("final X-180 cost = ", x_180_result.fun, "\n")
-
-########################################## Hadamard operation on system ##########################################
-
-single_hadamard_operation = (1 / np.sqrt(2)) * np.array([[1., 1.], [1., -1.]])
-hadamard_operation = broadcast_single_qubit_operation(single_hadamard_operation,
-                                                 qubit_count)
-hadamard_operation_result = run_optimization(hadamard_operation)
-hadamard_operation_signal = hadamard_operation_result.x
-print("final Hadamard cost = ", hadamard_operation_result.fun, "\n")
-
-########################################## Operations on isolated atoms ##########################################
-
-# FIXME: Produces the wrong operation
-
-# single_x_180_rotation = np.fliplr(np.eye(2))
-# single_hadamard_operation = (1 / np.sqrt(2)) * np.array([[1., 1.], [1., -1.]])
-# # Will apply an X-180 rotation to every other atom, and a Hadamard operation
-# # to the untouched atoms.
-# individual_operation_list = [None] * qubit_count
-# for qubit in range(qubit_count):
-#     if ((qubit % 2) != 0):
-#         individual_operation_list[qubit] = single_hadamard_operation
-#     else:
-#         individual_operation_list[qubit] = single_x_180_rotation
-# full_operation = broadcast_individual_operations(individual_operation_list)
-# print(full_operation)
-
-# zero = np.array([1.,0.,0.,0.])
-# print("\n", np.dot(full_operation, zero), "\n")
-
-# # result = run_optimization(full_operation)
-# # signal = result.x
-# # print("final mixed gates cost = ", result.fun, "\n")
-
-
-################################################################################################################
-
-# # Now let's optimize for a randomly generated Unitary matrix.
-# random_gate = stats.unitary_group.rvs(2**qubit_count)
-# random_result = run_optimization(random_gate)
-# random_signal = random_result.x
-# print("final cost = ", random_result.fun)
-
-# ################################################################################################################
-
-# # Make pretty pictures of the signals
-
-# time_values = np.linspace(0, T, chunks)
-
-# fig, axs = plt.subplots(3)
-# axs[0].set_title("Optimized X Signal")
-# axs[0].step(time_values, x_signal)
-# axs[1].set_title("Optimized Hadamard Signal")
-# axs[1].step(time_values, hadamard_signal)
-# axs[1].set_ylabel("signal Amplitude")
-# axs[2].set_title("Optimized Random Signal")
-# axs[2].step(time_values, random_signal)
-# axs[2].set_xlabel("Time\n(N_time_chunks * dt = T)")
-# fig.tight_layout(pad=1.0)
-
-# plt.savefig("out.png", bbox_inches="tight", pad_inches=0.5)
+qubit_count = 6
+energy_minimized_signals = run_optimization(qubit_count)
+print("final minimized energy = ", energy_minimized_signals.fun, "\n")
